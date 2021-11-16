bda0bf975790cd5019fa7967eb6cf8f8f2ee89da
BijanEmami <91327388+BijanEmami@users.noreply.github.com>
2021-11-15 18:38:51 -0600

    commit

---
+++ CIS_264_Price_APP/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace CIS_264_Price_APP.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CIS_264_Price_APP.Data.CIS_264_Price_APPContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(CIS_264_Price_APP.Data.CIS_264_Price_APPContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
