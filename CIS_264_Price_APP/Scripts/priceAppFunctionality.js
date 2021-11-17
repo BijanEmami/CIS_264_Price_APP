6783c13c681de0da52f9d3115a6613061d22df11
Lydia Weaver <lydiarose23@yahoo.com>
2021-11-16 22:51:22 -0600

    added js

---
+++ CIS_264_Price_APP/Scripts/priceAppFunctionality.js
@@ -0,0 +1,27 @@
+﻿sessionStorage.setItem("isAdmin", false);
+
+function toggleLogInOut() {
+    let protectedItems = document.querySelectorAll('.protected');
+
+    protectedItems.forEach(item => item.classList.toggle('disable'))
+
+    document.getElementById("login").classList.toggle('disable');
+    document.getElementById("logout").classList.toggle('disable');
+
+    sessionStorage.setItem("isAdmin", false);
+}
+
+function login() {
+    let pw = 'pwd';
+    let result = prompt('Enter password', '');
+
+    while (result != "pwd" && result != null) result = prompt('Incorrect Password, Try Again');
+
+    if (pw == 'pwd') {
+        toggleLogInOut();
+        sessionStorage.setItem("isAdmin", true);
+    }
+}
+
+document.getElementById("login").addEventListener("click", login);
+document.getElementById("logout").addEventListener('click', toggleLogInOut);
