d10ed8a2ddec840bb1e93a0aa0c770db1a1466fe
tsmith256 <62732586+tsmith256@users.noreply.github.com>
2021-11-27 12:34:13 -0600

    Update priceAppFunctionality.js
    Updated JavaScript page

--- CIS_264_Price_APP/Scripts/priceAppFunctionality.js
+++ CIS_264_Price_APP/Scripts/priceAppFunctionality.js
@@ -1,27 +1,55 @@
-﻿sessionStorage.setItem("isAdmin", false);
+function login() {
+  // tyler's code here for pop up
+
+  close.onclick = function() {
+    document.getElementById('popup').style.display = "none";
+  }
+  if (loginBtn.id == "login") {
+    document.getElementById('popupbox').style.visibility="visible";
+    document.getElementById('popup').style.display="block";
+    document.getElementById('submit').addEventListener('click', validate);
+  }
+  else if (loginBtn.id == "logout"){
+    toggleLogInOut();
+  }
+
+  //while (result != "pwd" && result != null) result = document.getElementById("placeholder").innerText;
+
+}
+
+function validate() {
+
+  let result = document.getElementById("password");
+  let placeholder = document.getElementById("placeholder");
+
+  if (result.value == pw) {
+    result.value = "";
+    document.getElementById('popupbox').style.visibility="hidden";
+    document.getElementById('popup').style.display="none";
+    toggleLogInOut();
+  }
+  else {
+    placeholder.innerText = "Incorrect Password";
+  }
+
+}
 
 function toggleLogInOut() {
-    let protectedItems = document.querySelectorAll('.protected');
-
-    protectedItems.forEach(item => item.classList.toggle('disable'))
-
-    document.getElementById("login").classList.toggle('disable');
-    document.getElementById("logout").classList.toggle('disable');
-
-    sessionStorage.setItem("isAdmin", false);
+  //let protectedItems = document.querySelectorAll('.protected');
+  //protectedItems.forEach(item => item.classList.toggle('disable'));
+  console.log(loginBtn.innerText);
+  if (loginBtn.id == "login") {
+    loginBtn.id="logout";
+    loginBtn.innerText="Logout";
+  }
+  else if (loginBtn.id == "logout"){
+    loginBtn.id="login";
+    loginBtn.innerText="Login";
+  }
+  console.log(loginBtn.innerText);
 }
 
-function login() {
-    let pw = 'pwd';
-    let result = prompt('Enter password', '');
-
-    while (result != "pwd" && result != null) result = prompt('Incorrect Password, Try Again');
-
-    if (pw == 'pwd') {
-        toggleLogInOut();
-        sessionStorage.setItem("isAdmin", true);
-    }
-}
-
-document.getElementById("login").addEventListener("click", login);
-document.getElementById("logout").addEventListener('click', toggleLogInOut);
+let loginBtn = document.getElementById('login');
+loginBtn.addEventListener('click', login);
+let close = document.getElementById('close');
+let pw = 'pwd';
